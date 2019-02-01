7d44237d723f35629562f4888bf747bba395e44a
Thomas Castiglione <castiglione@mac.com>
2019-02-02 01:06:09 +0800

    map the whole net, as far as i know

--- lib-servers.js
+++ lib-servers.js
@@ -4,10 +4,12 @@ export class Server {
     /**
      * @param {string} [name]
      * @param {number} [ram]
+     * @param {number} [ports]
      */
-    constructor(name, ram) {
+    constructor(name, ram, ports) {
         this.name = name;
         this.ram = ram;
+        this.ports = ports;
         this.job = '';
     }
 
@@ -27,51 +29,82 @@ export class Server {
 export function bots() {
     var all = [];
     for (var i = 1; i <= 20; i++) {
-        all.push(new Server('bot'+i, 4));
+        all.push(new Server('bot'+i, 4, 0));
     }
     return all;
 }
 
+// notes: traversal past depth 10 is via defcomm and then zb-def
 export function map() {
     return [
-        new Server('foodnstuff', 16), 
-        new Server('nectar-net', 16), 
-                new Server('neo-net', 32), 
-                    new Server('johnson-ortho', 0), 
-                    new Server('crush-fitness', 0), 
-                    new Server('avmnite-02h', 16), 
-                        new Server('I.I.I.I', 64), 
-                new Server('phantasy', 32), 
-                    new Server('the-hub', 8), 
-                        new Server('summit-uni',64), 
-                            new Server('rho-construction', 16), 
-                    new Server('syscore', 0), 
-                new Server('comptek', 0), 
-                    new Server('zb-institute', 16), 
-                        new Server('alpha-ent', 16), 
-                            new Server('galactic-cyber', 0), 
-                                new Server('deltaone', 0), 
-                            new Server('aerocorp', 0), 
-                            new Server('global-pharm', 64), 
-                        new Server('aevum-police', 64), 
-        new Server('sigma-cosmetics', 16), 
-        new Server('joesguns', 16), 
-            new Server('zer0', 32), 
-                new Server('silver-helix', 64),
-                    new Server('netlink', 16), 
-                        new Server('rothman-uni', 64), 
-                            new Server('lexo-corp', 128), 
-                                new Server('snap-fitness', 0), 
-                                    new Server('omnia', 64), 
-                                    new Server('unitalife', 32), 
-                            new Server('millenium-fitness', 128), 
-                        new Server('catalyst', 32), 
-        new Server('hong-fang-tea', 16), 
-        new Server('harakiri-sushi', 16), 
-            new Server('CSEC', 8), 
-                new Server('omega-net', 32), 
-        new Server('iron-gym', 32), 
-            new Server('max-hardware', 32), 
+        new Server('foodnstuff', 16, 0), 
+        new Server('nectar-net', 16, 0), 
+                new Server('neo-net', 32, 1), 
+                    new Server('johnson-ortho', 0, 2), 
+                    new Server('crush-fitness', 0, 2), 
+                    new Server('avmnite-02h', 16, 2), 
+                        new Server('I.I.I.I', 64, 3), 
+                new Server('phantasy', 32, 2), 
+                    new Server('the-hub', 8, 2), 
+                        new Server('summit-uni', 64, 3), 
+                            new Server('rho-construction', 16, 3), 
+                    new Server('syscore', 0, 4), 
+                new Server('comptek', 0, 3), 
+                    new Server('zb-institute', 16, 5), 
+                        new Server('alpha-ent', 16, 4), 
+                            new Server('galactic-cyber', 0, 5), 
+                                new Server('deltaone', 0, 5), 
+                                    new Server('defcomm', 0, 5), 
+                                        new Server('infocomm', 0, 5), 
+                                            new Server('microdyne', 32, 5), 
+                                        new Server('taiyang-digital', 0, 5), 
+                                            new Server('titan-labs', 32, 5), 
+                                        new Server('zb-def', 0, 4), 
+                                            new Server('applied-energetics', 0, 4), 
+                                                new Server('stormtech', 0, 5), 
+                                                    new Server('kuai-gong', 0, 5), 
+                                                        new Server('powerhouse-fitness', 16, 5), 
+                                                            new Server('The-Cave', 0, 5), 
+                                                    new Server('.', 16, 4), 
+                                                        new Server('nwo', 0, 5), 
+                                                new Server('helios', 128, 5), 
+                                                    new Server('4sigma', 0, 5), 
+                                                        new Server('b-and-a', 0, 5), 
+                                                        new Server('blade', 64, 5), 
+                                                            new Server('ecorp', 0, 5), 
+                                                            new Server('megacorp', 0, 5), 
+                                                            new Server('fulcrumassets', 0, 5), 
+                                            new Server('run4theh111z', 256, 4), 
+                                                new Server('fulcrumtech', 128, 5), 
+                                                new Server('vitalife', 16, 5), 
+                                                    new Server('omnitek', 256, 5), 
+                                                        new Server('clarkinc', 0, 5), 
+                                    new Server('univ-energy', 128, 4), 
+                                    new Server('solaris', 16, 5), 
+                                    new Server('zeus-med', 0, 5), 
+                                        new Server('nova-med', 0, 4), 
+                            new Server('aerocorp', 0, 5), 
+                            new Server('global-pharm', 64, 4), 
+                        new Server('aevum-police', 64, 4), 
+        new Server('sigma-cosmetics', 16, 0), 
+        new Server('joesguns', 16, 0), 
+            new Server('zer0', 32, 1), 
+                new Server('silver-helix', 64, 2),
+                    new Server('netlink', 16, 3), 
+                        new Server('rothman-uni', 64, 3), 
+                            new Server('lexo-corp', 128, 4), 
+                                new Server('snap-fitness', 0, 4), 
+                                    new Server('omnia', 64, 5), 
+                                    new Server('unitalife', 32, 4), 
+                                        new Server('icarus', 0, 5), 
+                            new Server('millenium-fitness', 128, 3), 
+                        new Server('catalyst', 32, 3), 
+        new Server('hong-fang-tea', 16, 0), 
+        new Server('harakiri-sushi', 16, 0), 
+            new Server('CSEC', 8, 1), 
+                new Server('omega-net', 32, 2), 
+        new Server('iron-gym', 32, 1), 
+            new Server('max-hardware', 32, 1), 
     ];
 }
 
