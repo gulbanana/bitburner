8fa19256015326391cb54954cd32c3a80250f004
Thomas Castiglione <castiglione@mac.com>
2019-02-05 15:48:15 +0800

    megaserver stop script

---
+++ ms-stop.js
@@ -0,0 +1,24 @@
+import { Logger } from './lib-log.js';
+import * as servers from './lib-servers.js';
+
+/** @param {IGame} ns */
+export async function main(ns) {
+    let log = new Logger(ns, { termInfo: true });
+
+    for (var bot of servers.bots(ns)) {
+        let idle = true;
+        
+        let scripts = ns.ps(bot.name);
+        for (let script of scripts) {
+            if (script.filename.startsWith('ms-')) {
+                log.info(`${bot.name}: ${script.args[0]} (kill)`);
+                ns.scriptKill(script.filename, bot.name);
+                idle = false;
+            }
+        }
+
+        if (idle) {
+            log.info(`${bot.name}: idle`);
+        }
+    }
+}
