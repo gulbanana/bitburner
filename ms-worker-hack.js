e143face20d72b926d7ce506d9982dbc0e596e8c
Thomas Castiglione <castiglione@mac.com>
2019-02-05 17:13:16 +0800

    bugfix for very quick ms cycles

--- ms-worker-hack.js
+++ ms-worker-hack.js
@@ -4,5 +4,8 @@
 export async function main(ns) {
     var target = ns.args[0];
     await ns.hack(target);
+    while (ns.scriptRunning('ms-control.js', ns.getHostname())) {
+        await ns.sleep(1000);
+    }
     await ns.exec('ms-control.js', ns.getHostname(), 1, target);
 }
